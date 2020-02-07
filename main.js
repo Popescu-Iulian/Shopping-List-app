e178297becf7f89d8a4659ae272a495afdb26f8a
Popescu-Iulian <popescu.iulian@protonmail.com>
2020-02-07 16:42:29 +0200

    js code

---
+++ main.js
@@ -0,0 +1,46 @@
+const ITEMS_LIST = document.querySelector('#items-list');
+const ITEMS_INPUT = document.querySelector('#items-input');
+
+const LIST = [
+	{
+		item: 'ion'
+	}
+];
+
+function draw() {
+	let liItem = '';
+
+	for (let i = 0; i < LIST.length; i++) {
+		liItem += `
+      <li>${LIST[i].item}<span class="delete" onclick="delItem(${i})">X</span></li>
+    `;
+	}
+
+	ITEMS_LIST.innerHTML = liItem;
+}
+
+function addItem(event) {
+	event.preventDefault();
+
+	const NEW_ITEM = {
+		item: ITEMS_INPUT.value
+	};
+
+	if (ITEMS_INPUT.value === '') {
+		alert(`Please enter an item to buy!`);
+	} else {
+		LIST.push(NEW_ITEM);
+	}
+
+	ITEMS_INPUT.value = '';
+
+	draw();
+}
+
+function delItem(idx) {
+	if (confirm(`You don't want to buy ${LIST[idx].item}?`)) {
+		LIST.splice(idx, 1);
+
+		draw();
+	}
+}
